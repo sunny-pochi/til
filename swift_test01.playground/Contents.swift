da5b4cf251bf9288ec04b2e356251cea968e385b
sht <>
2022-02-17 20:32:03 +0900

    書き方確認

---
+++ swift_test01.playground/Contents.swift
@@ -0,0 +1,92 @@
+import UIKit
+
+//var greeting = "Hello, playground"
+//1 + 2
+//
+//// 変数の宣言　初期値がない場合は、型を指定
+//var title: String = "為替計算"
+//var dollar:Double = 125
+//var rate:Double = 112.3
+//
+//var yen = dollar * rate
+//
+//// 変更のない値：定数はlet を使用する
+//let title_Retake: String = "為替計算２"
+//var dollar_Retake:Double = 125
+//var rate_Retake:Double = 112.3
+//
+//let yen_Retake = dollar_Retake * rate_Retake
+//
+//let name2 = "太田"
+//let who = name + " 殿"
+//
+//var starsBig = ""
+//starsBig += "★"
+//starsBig += "☆"
+//starsBig += "★"
+//
+//let tanka = 100
+//let suuryo = 10
+//let amount = String(tanka * suuryo) + "円です"
+//
+//let time = 9.98
+//let result = "タイムは\(time)秒でした"
+
+// タプル
+//var country = ("アメリカ", "イギリス", "ロシア")
+//var country:(String, String, String) = ("アメリカ", "イギリス", "ロシア")
+//
+////var morimoto = ("森本", "鹿児島", "31")
+//var morimoto:(String, String, Int)
+//morimoto = ("森本", "もりもと", 31)
+//
+//let (name, _, age) = morimoto
+//let user = name + "ちゃん、" + "\(age)歳"
+//
+//let numList = [3, 4, 7, 8, 9, 12, 15]
+//var sum = 0
+//for num in numList {
+//    sum += num
+//}
+//print(sum)
+
+var circle = ""
+for _ in 1 ... 5 {
+    circle += "●"
+    print(circle)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
