08e168da03017bbb4374a666c81e00d2e589ff71
sht <>
2022-02-19 19:32:47 +0900

    学習ログ続き　論理値

---
+++ SwiftTest_03.playground/Contents.swift
@@ -0,0 +1,139 @@
+import UIKit
+
+//print(20 + 10)
+//// 30
+//print(20 - 10)
+//// 10
+//print(20 * 10)
+//// 200
+//print(20 / 10)
+//// 2
+//print(10 % 3)
+//// 1
+
+
+//var point: Int = 300
+//var minusPoint: Int = -point
+//var plusPoint: Int = -minusPoint
+//
+//print(minusPoint)
+//print(plusPoint)
+
+//print(300 == 300)
+//// true
+//print(300 == 200)
+//// false
+//print("アップル" == "アップル")
+//// true
+//print("アップル" == "apple")
+//// false
+//print(true == true)
+//// true
+//print(true == false)
+//// false
+//print(300 != 300)
+//// false
+//print(300 != 200)
+//// true
+//print("アップル" != "アップル")
+//// false
+//print("アップル" != "apple")
+//// true
+//print(true != true)
+//// false
+//print(false != false)
+//// false
+//
+//var firstPoint = 300
+//var secondPoint = 300
+//
+//print(firstPoint == secondPoint)
+//// true
+//print(firstPoint != secondPoint)
+//// false
+//
+//print(firstPoint > secondPoint)
+//// false
+//print(firstPoint < secondPoint)
+//// false
+//print(firstPoint >= secondPoint)
+//// true
+//print(firstPoint <= secondPoint)
+//// true
+
+//class Person {
+//    var name: String
+//    init(name: String) {
+//        self.name = name
+//    }
+//}
+//
+//let tarou = Person(name: "たろう")
+////let jirou = Person(name: "じろう")
+//let copyTarou = tarou
+//
+//print(tarou === copyTarou)
+//print(tarou !== copyTarou)
+//print(ObjectIdentifier(tarou))
+//print(ObjectIdentifier(copyTarou))
+
+//var name: String? = nil
+//print(name ?? "名無しさん")
+//name = "はなこ"
+//print(name ?? "名無しさん")
+
+//print(Int.random(in: 1...5))
+//
+//for index in 1..<5 {
+//    print("\(index)回目の繰り返し")
+//}
+
+//var areaAData = true
+//var areaBData = true
+//print(areaAData)
+//print(areaBData)
+//
+//if areaAData && areaBData {
+//    print("ともにtrue")
+//} else {
+//    print("ともにtrueではない")
+//}
+//if areaAData || areaBData {
+//    print("どちらかがtrue")
+//} else {
+//    print("どちらもtrueではない")
+//}
+//print("==== ここから変更 =====")
+//
+//areaAData = false
+//print(areaAData)
+//print(areaBData)
+//if areaAData && areaBData {
+//    print("ともにtrue")
+//} else {
+//    print("ともにtrueではない")
+//}
+//if areaAData || areaBData {
+//    print("どちらかがtrue")
+//} else {
+//    print("どちらもtrueではない")
+//}
+
+//let tarouPoint = 300
+//let jirouPoint = 150
+//tarouPoint < jirouPoint ? print("たろう勝利"): print("じろう勝利")
+let selected = true
+print(selected ? "成功" : "失敗")
+
+
+
+
+
+
+
+
+
+
+
+
+
